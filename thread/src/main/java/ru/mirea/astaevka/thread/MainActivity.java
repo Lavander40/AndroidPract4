89891f128f212eca3535dd0d8e055a3285719135
Lavander40 <a.kirill.03@mail.ru>
2023-04-14 00:35:56 +0300

    ?

--- thread/src/main/java/ru/mirea/astaevka/thread/MainActivity.java
+++ thread/src/main/java/ru/mirea/astaevka/thread/MainActivity.java
@@ -3,12 +3,54 @@ package ru.mirea.astaevka.thread;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Arrays;
+
+import ru.mirea.astaevka.thread.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ActivityMainBinding binding;
+    private int counter = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        TextView infoTextView = binding.textView2;
+        Thread mainThread = Thread.currentThread();
+        infoTextView.setText("Имя текущего потока: " + mainThread.getName());
+        mainThread.setName("МОЙ НОМЕР ГРУППЫ: 03, НОМЕР ПО СПИСКУ: 02, МОЙ ЛЮБИИМЫЙ ФИЛЬМ: Назад в будущее");
+        infoTextView.append("\n Новое имя потока: " + mainThread.getName());
+        Log.d(MainActivity.class.getSimpleName(), "Group: " + mainThread.getThreadGroup());
+        Log.d(MainActivity.class.getSimpleName(), "Stack: " + Arrays.toString(mainThread.getStackTrace()));
+        binding.button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                new Thread(new Runnable() {
+                    public void run() {
+                        int numberThread = counter++;
+                        Log.d("ThreadProject", String.format("Запущен поток No %d студентом группы No %s номер по списку No %d ", numberThread, "БСБО-03-20", 2));
+                        long endTime = System.currentTimeMillis() + 20 * 1000;
+                        while (System.currentTimeMillis() < endTime) {
+                            synchronized (this) {
+                                try {
+                                    wait(endTime - System.currentTimeMillis());
+                                    Log.d(MainActivity.class.getSimpleName(), "Endtime: " + endTime);
+                                } catch (Exception e) {
+                                    throw new RuntimeException(e);
+                                }
+                            }
+                            Log.d("ThreadProject", "Выполнен поток No " + numberThread);
+                        }
+                        binding.textView.setText((Integer.parseInt(String.valueOf(binding.editTextTextPersonName.getText())) / Integer.parseInt(String.valueOf(binding.editTextTextPersonName2.getText()))));
+                    }
+                }).start();
+            }
+        });
     }
 }
