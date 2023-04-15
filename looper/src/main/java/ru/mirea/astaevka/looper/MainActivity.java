a4748f3b4b5add1882ee458e64500650189a8bc2
Lavander40 <a.kirill.03@mail.ru>
2023-04-15 21:42:54 +0300

    looper

---
+++ looper/src/main/java/ru/mirea/astaevka/looper/MainActivity.java
@@ -0,0 +1,44 @@
+package ru.mirea.astaevka.looper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.util.Log;
+import android.view.View;
+
+import ru.mirea.astaevka.looper.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        Handler mainThreadHandler = new Handler(Looper.getMainLooper()) {
+            @Override
+            public void handleMessage(Message msg) {
+
+                Log.d(MainActivity.class.getSimpleName(), "Task execute. This is result: " + msg.getData().getString("result"));
+            }
+        };
+        MyLooper myLooper = new MyLooper(mainThreadHandler);
+        myLooper.start();
+        binding.editTextMirea.setText("Мой номер по списку No2");
+        binding.buttonMirea.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Message msg = Message.obtain();
+                Bundle bundle = new Bundle();
+                bundle.putString("KEY", "Astaev");
+                msg.setData(bundle);
+                myLooper.mHandler.sendMessage(msg);
+            }
+        });
+    }
+}
